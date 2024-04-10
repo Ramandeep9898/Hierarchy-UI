c8d5deecd45f9f55a081e11dea2dca9235182213
Ramandeep <ramank6238299@gmail.com>
2024-04-10 19:23:46 +0530

    added react query + router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route, Routes } from "react-router-dom";
+
+const Home = () => {
+  return <>home</>;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/charts" element={<Home />} />
+      </Routes>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
